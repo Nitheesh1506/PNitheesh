e051eeb00f0139480e6fb5fbebef67b8ec7bbd7a
PayyavulaNitheesh <nitheesh.p@capgemini.com>
2022-11-25 16:45:10 +0530

    first commit

---
+++ src/main/java/com/OnlineMedicalShop/OnlineMedicalShopApplication.java
@@ -0,0 +1,14 @@
+package com.OnlineMedicalShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMedicalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMedicalShopApplication.class, args);
+		System.out.println("Server running");
+	}
+
+}
